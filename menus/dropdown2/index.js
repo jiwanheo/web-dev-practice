c592b3449cd50a00e955c44278fe2a7f551ab17e
jiwanheo <jiwanheo123@gmail.com>
2020-08-27 20:43:53 -0400

    I suck at Javascript

---
+++ menus/dropdown2/index.js
@@ -0,0 +1,6 @@
+const menuBtnToggle = document.querySelector('.menu-label');
+const navItems = document.querySelector('.nav-items');
+
+menuBtnToggle.addEventListener('click', () => {
+	navItems.classList.toggle('ahduashiohd')
+})
